4472414b7a9d1d01d6df4421e02af084bfeaf22f
Rafael <tauschek.rafael@gmail.com>
2024-04-19 14:31:16 +0200

    Video: Category and Genre

---
+++ videoflix/video/choices.py
@@ -0,0 +1,30 @@
+GENRE_CHOICES = [
+    ('Thriller', 'Thriller'),
+    ('Fantasy', 'Fantasy'),
+    ('History', 'History'),
+    ('Comedy', 'Comedy'),
+    ('Adventure', 'Adventure'),
+    ('Drama', 'Drama'),
+    ('Western', 'Western'),
+    ('Science fiction', 'Science fiction'),
+    ('Documentary', 'Documentary'),
+    ('Crime', 'Crime'),
+    ('Mystery', 'Mystery'),
+    ('Romance', 'Romance'),
+    ('Satire', 'Satire'),
+    ('Horror', 'Horror'),
+    ('Sport', 'Sport')
+]
+
+FSK_CHOICES = [
+    (0, '0'),
+    (6, '6'),
+    (12, '12'),
+    (16, '16'),
+    (18, '18')
+]
+
+CATEGORY_CHOICES = [
+    ('Film', 'Film'),
+    ('Show', 'Show')
+]
--- videoflix/video/models.py
+++ videoflix/video/models.py
@@ -1,14 +1,9 @@
 import datetime
 from django.db import models
+from .choices import *
 
 class Video(models.Model):
-    FSK_CHOICES = [
-        (0, '0'),
-        (6, '6'),
-        (12, '12'),
-        (16, '16'),
-        (18, '18')
-    ]
+
     title = models.CharField(max_length=200, default='', blank=False)
     description = models.TextField(max_length=1000, default='', blank=False)
     video_file = models.FileField(null=True, blank=True, upload_to='videos')
@@ -17,6 +12,8 @@ class Video(models.Model):
     thumbnail = models.FileField(null=True, blank=True, upload_to='thumbnails')
     genre = models.CharField(max_length=100, blank=True)
     duration_time = models.IntegerField(blank=True, null=True, default=0)
+    genre = models.CharField(choices=GENRE_CHOICES, default='', blank=True, null=True, max_length=100)
+    category = models.CharField(choices=CATEGORY_CHOICES, default='', blank=True, null=True, max_length=100)
 
     def __str__(self):
         return self.title
