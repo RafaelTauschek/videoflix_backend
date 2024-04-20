42a9b439df165477fa47aae9e56def86f02b97c1
Max Wirnsberger <maximilian.wirnsberger@gmail.com>
2024-04-20 17:47:54 +0200

    added logic to reset password

--- videoflix/user/urls.py
+++ videoflix/user/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from .views import EditUserView, RegisterView, ConfirmEmailView, LoginUserView, LogoutView, ChangePasswordView
+from .views import EditUserView, RegisterView, ConfirmEmailView, LoginUserView, LogoutView, ChangePasswordView, ForgotPasswordView, PasswordResetRequestView
 
 urlpatterns = [
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('edit-user/', EditUserView.as_view(), name='edit_user'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('reset-password/', ChangePasswordView.as_view(), name='reset-password'),
+    path('reset-password/', ChangePasswordView.as_view(), name='reset_password'),
+    path('reset-password-mail/', PasswordResetRequestView.as_view(), name='reset_password_mail'),
     path('confirm-email/<uidb64>/<token>/', ConfirmEmailView.as_view(), name='confirm_email'),
+    path('password-reset-confirm/<uidb64>/<token>/', ForgotPasswordView.as_view(), name='password_reset_confirm'),
 ]
 
 
