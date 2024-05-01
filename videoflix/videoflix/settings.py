857a6784e883d0d4208d7c16433471e42ed48c39
Rafael <tauschek.rafael@gmail.com>
2024-05-01 09:51:01 +0200

    requirements update

--- videoflix/videoflix/settings.py
+++ videoflix/videoflix/settings.py
@@ -103,30 +103,18 @@ WSGI_APPLICATION = 'videoflix.wsgi.application'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'admin0815',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
-# Boilerplate for Postgresql
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': '',
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': '',
-#         'PORT': '',
-#     }
-# }
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
